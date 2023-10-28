f61043cec74d0cea3539ecabda02259dc0bd6a11
nataliaheine <136356193+nataliaheine@users.noreply.github.com>
2023-10-28 20:48:59 +0200

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -7,7 +7,7 @@ st.header("PORTFOLIO")
 
 projekte = {
   "Toxic Comments Classification":toxic_comments, 
-  "AutoScout24 Car Price Prediction":None]
+  "AutoScout24 Car Price Prediction":None}
 select = st.radio("", list(projekte(keys()), horizontal=True)
 
 if select = "Toxic Comments Classification":
