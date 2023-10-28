e14e855bb5a71d239dac2e35f847e9373b093efe
nataliaheine <136356193+nataliaheine@users.noreply.github.com>
2023-10-28 20:48:31 +0200

    Update portfolio.py
    .

--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import toxic_comments
+from toxic_comments import home
 
 st.title("Natalia Heine")
 st.header("PORTFOLIO")
 
-projekte = ["Toxic Comments Classification", "AutoScout24 Car Price Prediction"]
-st.radio("", projekte, horizontal=True)
+projekte = {
+  "Toxic Comments Classification":toxic_comments, 
+  "AutoScout24 Car Price Prediction":None]
+select = st.radio("", list(projekte(keys()), horizontal=True)
+
+if select = "Toxic Comments Classification":
+home.start()
